63f5e9a08f562688cc7597fb2e0b52c5c6ee995c
Christof Urbaczek <christof.urbaczek@student.kit.edu>
2016-11-30 18:53:33 +0100

    Add analysis script

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 build_folder := build
 
-run_main := python main.py
+run_main := python traffic_index.py
 run_quick_tests := ./.quality-checks/pep8.sh
 run_detailed_tests := ./.quality-checks/pep8-detailed.sh
 
@@ -15,3 +15,8 @@ clean:
 	rm -Rf $(build_folder)
 	find . -name "*.pyc" -exec rm {} \;
 	find . -name "*~" -exec rm {} \;
+
+karlsruhe:
+	mkdir -p $(build_folder)
+	$(run_main) --city Karlsruhe --lat 49.0068900 --lng 8.4036530 --zoom 14 --debug --stdout
+
---
+++ traffic_index.py
@@ -0,0 +1,83 @@
+from __future__ import division
+import os
+import yaml
+import distutils
+from distutils import dir_util
+
+from lib import model
+from lib.util.file import clear_dir
+from lib.util.file import copy_dir
+
+DEFAULT_BUILD_DIRECTORY = 'build'
+LOCATION_DATA_DIRECTORY = 'data/locations'
+RES_DIRECTORY = 'res'
+
+
+def print_result_to_stdout(result):
+    # TODO
+    print("*** Analysis Result ***")
+    print(result)
+
+
+def generate_html(result, build_directory):
+    # TODO
+    print("[ERROR] html output not yet implemented")
+    #html_generator.generate_html(build_directory, RES_DIRECTORY, result)
+
+
+def analyse_city(name, lat, lng, zoom_level):
+    # TODO
+    return model.Location(name, lat, lng, None)
+
+
+def get_parser():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='show debug information')
+    parser.add_argument('--stdout',
+                        action='store_true',
+                        help="print to stdout")
+    parser.add_argument('--target-folder',
+                        type=str,
+                        dest='target_folder',
+                        default=DEFAULT_BUILD_DIRECTORY,
+                        help="target folder for generated files")
+    parser.add_argument('--city',
+                        type=str,
+                        help="name of the city to be analyzed",
+                        required=True)
+    parser.add_argument('--lat',
+                        type=str,
+                        help="latitude of the city",
+                        required=True)
+    parser.add_argument('--lng',
+                        type=str,
+                        help="longitude of the city",
+                        required=True)
+    parser.add_argument('--zoom',
+                        type=int,
+                        help="zoom level to analyse",
+                        required=True)
+    parser.add_argument('--cache', 
+                        type=str,
+                        dest='cache_file')
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+
+    if args.cache_file and os.path.isfile(args.cache_file):
+        location_result = yaml.load(open(args.cache_file, 'r'))
+    else:
+        location_result = analyse_city(args.city, args.lat, args.lng, args.zoom)
+        if args.cache_file:
+            yaml.dump(location_result, open(args.cache_file, 'w'))
+
+    if args.stdout:
+        print_result_to_stdout(location_result)
+    else:
+        generate_html(location_result, args.target_folder)
